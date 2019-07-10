8dca7e2bf1beb7f0b43357f3f310047c693a9864
dxj <dengsinkiang@gmail.com>
2019-07-10 17:56:52 +0800

    date time

--- src/views/log/login.vue
+++ src/views/log/login.vue
@@ -68,11 +68,14 @@ export default {
       function GMTToStr(time) {
         const date = new Date(time)
         return date.getFullYear() + '-' +
-          (date.getMonth() + 1).toString().padStart(2, 0) + '-' +
-          date.getDate().toString().padStart(2, 0)
+          (date.getMonth() + 1).toString().padStart(2, '0') + '-' +
+          date.getDate().toString().padStart(2, '0') + ' ' +
+          date.getHours().toString().padStart(2, '0') + ':' +
+          date.getMinutes().toString().padStart(2, '0') + ':' +
+          date.getSeconds().toString().padStart(2, '0')
       }
 
-      if (daterange && daterange) { this.params['startTime'] = GMTToStr(daterange[0]); this.params['endTime'] = GMTToStr(daterange[1]) }
+      if (daterange && daterange) { this.params['startDate'] = GMTToStr(daterange[0]); this.params['endDate'] = GMTToStr(daterange[1]) }
       return true
     }
   }
