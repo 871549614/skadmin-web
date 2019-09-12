a409ee7b5e61ab7ddba2cbe3c5c302bc96ad129a
dxj <dengsinkiang@gmail.com>
2019-09-12 17:57:30 +0800

    dxj config

--- src/views/tool/oss/module/config.vue
+++ src/views/tool/oss/module/config.vue
@@ -1,13 +1,14 @@
 <template>
   <el-form ref="form" :model="form" :rules="rules" style="margin-top: 6px;" size="small" label-width="130px">
-    <el-form-item label="服务提供商" prop="serviceProvider"><el-select v-model="oss.serviceName" placeholder="请选择" style="width: 200px" @on-change="changeOss">
-      <el-option value="LOCAL_OSS">本地服务器</el-option>
-      <el-option value="MINIO_OSS">MinIO</el-option>
-      <el-option value="QINIU_OSS">七牛云</el-option>
-      <el-option value="ALI_OSS">阿里云</el-option>
-      <el-option value="TENCENT_OSS">腾讯云</el-option>
+    <el-form-item label="服务提供商" prop="serviceProvider">
+      <el-select v-model="value" placeholder="请选择" style="width: 200px" @on-change="changeOss">
+        <el-option
+          v-for="item in options"
+          :key="item.value"
+          :label="item.label"
+          :value="item.value"/>
     </el-select></el-form-item>
-    <div v-if="oss.serviceName !== 'LOCAL_OSS'">
+    <div v-if="value !== 'LOCAL_OSS'">
       <el-form-item label="accessKey：" prop="accessKey">
         <el-input v-model="oss.accessKey" type="text" placeholder="请输入accessKey" style="width: 350px"/>
       </el-form-item>
@@ -16,7 +17,7 @@
           <el-input v-model="oss.secretKey" :readonly="!changedOssSK" class="input-see" type="text" icon="ios-eye" placeholder="请输入secretKey" style="width: 350px" @on-click="seeSecret(oss.serviceName)"/>
         </el-tooltip>
       </el-form-item>
-      <el-form-item v-if="oss.serviceName === 'QINIU_OSS'" label="zone存储区域：" prop="zone">
+      <el-form-item v-if="value === 'QINIU_OSS'" label="zone存储区域：" prop="zone">
         <el-select v-model="oss.zone" placeholder="请选择存储区域" style="width: 350px">
           <el-option value="-1">自动判断</el-option>
           <el-option value="0">华东</el-option>
@@ -29,7 +30,7 @@
       <el-form-item label="bucket空间：" prop="bucket">
         <el-select v-model="oss.bucket" type="text" placeholder="请输入bucket空间名" style="width: 350px"/>
       </el-form-item>
-      <el-form-item v-if="oss.serviceName=='TENCENT_OSS'" label="所属地域：" prop="bucketRegion">
+      <el-form-item v-if="value === 'TENCENT_OSS'" label="所属地域：" prop="bucketRegion">
         <el-select v-model="oss.bucketRegion" placeholder="请选择或输入搜索" style="width: 350px" filterable>
           <el-option v-for="(item, i) in dictBucketRegions" :key="i" :value="item.value">{{ item.title }}</el-option>
         </el-select>
@@ -49,9 +50,12 @@
       </el-form-item>
       <el-form-item :error="errorMsg" label="文件预览接口：" prop="endpoint">
         <el-input v-model="oss.endpoint" type="text" placeholder="请输入文件预览接口" style="width: 350px">
-          <el-select slot="prepend" v-model="oss.http" style="width: 80px" prop="http">
-            <el-option value="http://">http://</el-option>
-            <el-option value="https://">https://</el-option>
+          <el-select slot="prepend" v-model="value1" style="width: 80px" prop="http">
+            <el-option
+              v-for="item in options1"
+              :key="item.value1"
+              :label="item.label1"
+              :value="item.value1"/>
           </el-select>
         </el-input>
       </el-form-item>
@@ -67,6 +71,31 @@ export default {
   data() {
     return {
       form: 'oss',
+      options: [{
+        value: 'LOCAL_OSS',
+        label: '本地服务器'
+      }, {
+        value: 'MINIO_OSS',
+        label: 'MinIO'
+      }, {
+        value: 'QINIU_OSS',
+        label: '七牛云'
+      }, {
+        value: 'ALI_OSS',
+        label: '阿里云'
+      }, {
+        value: 'TENCENT_OSS',
+        label: '腾讯云'
+      }],
+      value: 'QINIU_OSS',
+      options1: [{
+        value1: 'http://',
+        label1: 'http://'
+      }, {
+        value1: 'https://',
+        label1: 'https://'
+      }],
+      value1: '',
       errorMsg: '',
       errorMsg2: '',
       oss: {
